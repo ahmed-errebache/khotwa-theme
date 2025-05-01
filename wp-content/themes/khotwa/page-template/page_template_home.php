6c029fc82bfde31144e721fd6e2d0291a3f5e4fa
Ahmed Errebache <ahmed.errebache1@gmail.com>
2025-05-01 22:32:45 +0200

    blog page

--- wp-content/themes/khotwa/page-template/page_template_home.php
+++ wp-content/themes/khotwa/page-template/page_template_home.php
@@ -26,17 +26,6 @@ get_template_part('template-parts/home/reviews_home');
 ?>
 
 
-<!-- Affichage du contenu principal de la page -->
-<?php
-if (have_posts()) :
-    while (have_posts()) : the_post();
-        the_content();
-    endwhile;
-endif;
-?>
-
-
-
 <?php
 get_footer(); // Récupère le footer.php du thème
 ?>
